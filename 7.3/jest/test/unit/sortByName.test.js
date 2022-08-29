dfebcda87a82c932d4c3a364eb84d8c7cf2fb006
Yuri Kopshev <yura.kopshev@gmail.com>
2022-08-29 12:15:44 +0200

    added new tests in jest and playwright

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -15,3 +15,26 @@ describe("Books names test suit", () => {
     ]);
   });
 });
+it("Books names should be sorted in ascending order where books were sorted", () => {
+    expect(
+        sorting.sortByName([
+            "Антогонист",
+            "Властелин Колец",
+        ])
+    ).toEqual([
+        "Антогонист",
+        "Властелин Колец",
+    ]);
+});
+it("Books names should be sorted in ascending order where books same", () => {
+    expect(
+        sorting.sortByName([
+            "Властелин Колец",
+            "Властелин Колец",
+        ])
+    ).toEqual([
+        "Властелин Колец",
+        "Властелин Колец",
+    ]);
+});
+
--- 7.3/playwright/NetologyTest.js
+++ 7.3/playwright/NetologyTest.js
@@ -1,16 +1,40 @@
-const { chromium } = require("playwright");
+const user = require('../playwright/user');
+const { chromium } = require('playwright');
+const { expect } = require("@playwright/test");
 
+//positive test
 (async () => {
   const browser = await chromium.launch({
     headless: false,
     slowMo: 5000,
-    devtools: true
   });
   const page = await browser.newPage();
   await page.goto("https://netology.ru");
-  await page.click("text=Каталог курсов");
-  await page.pause();
+  await page.click('text=Войти');
 
-  //assertion
+  await page.fill('[placeholder="Email"]', user.login);
+  await page.fill('[placeholder="Пароль"]', user.pass);
+  //await page.click('text=Войти');
+  await page.click('[data-testid="login-submit-btn"]');
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await browser.close();
+})();
+
+//negative test
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+    slowMo: 5000,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("https://netology.ru");
+  await page.click('text=Войти');
+  await page.fill('[placeholder="Email"]', 'ivan34@mail.ru');
+  await page.fill('[placeholder="Пароль"]', '12345');
+  await page.click('[data-testid="login-submit-btn"]');
+  await expect(
+      page.locator(".src-components-ui-Form-Hint--hint--AcxqM")
+  ).toBeVisible();
   await browser.close();
 })();
